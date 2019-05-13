10339762756ce55319d5abeb90d870859d2bb784
prajwollamichhane11 <prajwollamichhane11@gmail.com>
2019-05-13 23:56:39 +0545

    Cosine Similarity

---
+++ Codes/test.py
@@ -0,0 +1,36 @@
+import scipy.spatial
+import math
+from collections import Counter
+
+def build_vector(iterable1, iterable2):
+	counter1 = Counter(iterable1)
+	counter2 = Counter(iterable2)
+	all_items = set(counter1.keys()).union(set(counter2.keys()))
+	vector1 = [counter1[k] for k in all_items]
+	vector2 = [counter2[k] for k in all_items]
+	return vector1, vector2
+
+# finding the cosine similarity between the topics
+def cosim(v1, v2):
+	dot_product = sum(n1 * n2 for n1, n2 in zip(v1, v2) )
+	magnitude1 = math.sqrt(sum(n ** 2 for n in v1))
+	magnitude2 = math.sqrt(sum(n ** 2 for n in v2))
+	if((magnitude1 * magnitude2) != 0):
+		return dot_product / (magnitude1 * magnitude2)
+	else:
+		return 0
+
+file = open('topics.txt', 'r', encoding="UTF-8")
+print(file)
+for lines in file:
+	line = lines.split()
+	x = x + 1
+	nextFile = open('topics.txt', 'r', encoding="UTF-8")
+	for lines2 in nextFile:
+		line2 = lines2.split()
+		y = y + 1
+		v1, v2 = build_vector(line, line2)
+		print(cosim(v1, v2))
+
+print(x)
+print(y)
