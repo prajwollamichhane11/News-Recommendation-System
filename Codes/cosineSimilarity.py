dea396d9fc3e4d2580733ec1167af17a6e614ace
prajwollamichhane11 <prajwollamichhane11@gmail.com>
2019-05-22 07:37:49 +0545

    Cosine Done

--- Codes/cosineSimilarity.py
+++ Codes/cosineSimilarity.py
@@ -1,39 +1,39 @@
-import nltk, string
-from sklearn.feature_extraction.text import TfidfVectorizer
+import scipy.spatial
+import math
+from collections import Counter
 
+x = 0
+y = 0
+def build_vector(iterable1, iterable2):
+	counter1 = Counter(iterable1)
+	counter2 = Counter(iterable2)
+	all_items = set(counter1.keys()).union(set(counter2.keys()))
+	vector1 = [counter1[k] for k in all_items]
+	vector2 = [counter2[k] for k in all_items]
+	return vector1, vector2
 
-nltk.download('punkt')
+# finding the cosine similarity between the topics
+def cosim(v1, v2):
+	dot_product = sum(n1 * n2 for n1, n2 in zip(v1, v2) )
+	magnitude1 = math.sqrt(sum(n ** 2 for n in v1))
+	magnitude2 = math.sqrt(sum(n ** 2 for n in v2))
+	if((magnitude1 * magnitude2) != 0):
+		return dot_product / (magnitude1 * magnitude2)
+	else:
+		return 0
 
-
-stemmer = nltk.stem.porter.PorterStemmer()
-remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
-
-
-def stem_tokens(tokens):
-    return [stemmer.stem(item) for item in tokens]
-
-
-def normalize(text):
-    return stem_tokens(nltk.word_tokenize(text.lower().translate(remove_punctuation_map)))
-
-
-
-vectorizer = TfidfVectorizer(tokenizer=normalize, stop_words='english')
-
-
-
-def cosine_sim(text1, text2):
-    tfidf = vectorizer.fit_transform([text1, text2])
-    return ((tfidf * tfidf.T).A)[0,1]
-
-
-count = 0
-Title = open("./NewsTitle", "r")
-for lines in (Title):
-    count = count + 1
-    print(count,end = " ")
-    title_file = open("./NewsTitle", "r")
-    for eachline in (title_file):
-        print (eachline)
-        #print(lines)
-        print(cosine_sim(lines, eachline))
+file = open('News', 'r', encoding="UTF-8")
+for lines in file:
+        line = lines.split()
+        x = x + 1
+        nextFile = open('News', 'r', encoding="UTF-8")
+        for lines2 in nextFile:
+                line2 = lines2.split()
+                y = y + 1
+                v1, v2 = build_vector(line, line2)
+                similarity = cosim(v1, v2)
+                if similarity > 0.70:
+                        f = open("DisimilarNews.txt","a+")
+                        f.write(lines)
+print(x)
+print(y)
